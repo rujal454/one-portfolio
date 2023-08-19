4bfba716d1cbe3bba7ba6ee2f37f28d9331463de
rujal <rujalbangdiwala@gmail.com>
2023-08-19 18:19:53 +0530

    commit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -66,11 +66,11 @@ const Experience = () => {
       className="bg-gradient-to-b from-gray-800 to-black w-full h-screen flex flex-col justify-center items-center md:items-start p-8"
     >
       <div className="max-w-screen-lg mx-auto w-full text-white text-center md:text-left">
-        <div className="mb-6">
-          <p className="text-3xl md:text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
+        <div className="mt-16">
+          <p className="text-3xl md:text-4xl font-bold border-b-4 border-gray-500 p-2 inline ">
             Experience
           </p>
-          <p className="py-4">These are the technologies I've worked with</p>
+          <p className="py-4 ">These are the technologies I've worked with</p>
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
