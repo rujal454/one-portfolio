7e33ee32f456f62cc29e10cc959e26df10289393
rujal <rujalbangdiwala@gmail.com>
2023-08-19 16:55:53 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from "./components/Home";
 import SocialLink from "./components/SocialLink";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
 function App() {
   return (
     <div>
@@ -11,6 +13,8 @@ function App() {
       <SocialLink/>
       <About/>
       <Portfolio/>
+      <Experience/>
+      <Contact/>
     </div>
   );
 }
